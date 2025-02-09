6a3486a076ddf2a2126582ad0d4600a8d968a189
Meph3w <65207295+Meph3w@users.noreply.github.com>
2025-02-09 18:18:20 -0300

    Create markdownToHtml.js

---
+++ app/markdownToHtml.js
@@ -0,0 +1,9 @@
+// /utils/markdownToHtml.js
+import remark from 'remark';
+import html from 'remark-html';
+
+// Função para converter Markdown em HTML
+export function convertMarkdownToHtml(markdown) {
+  const result = remark().use(html).processSync(markdown);
+  return result.toString();
+}
