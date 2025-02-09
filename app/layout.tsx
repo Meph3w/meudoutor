adf16fb6a8588b8c3bc28e06377c3bef10768353
Meph3w <65207295+Meph3w@users.noreply.github.com>
2025-02-09 17:58:26 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,27 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        {/* Adicionar o link explícito para o favicon */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WV77RWCV');</script>
+<!-- End Google Tag Manager -->
+            `,
+          }}
+        />
         <link rel="icon" href="/favicon.png" />
       </head>
       <body className={`${inter.className} flex flex-col min-h-screen`}>
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV77RWCV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+        
         <Providers>
           <Header />
           <main className="flex-grow bg-[#161616]">{children}</main>
